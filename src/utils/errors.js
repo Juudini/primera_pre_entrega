740738a259b9cb6225de4af3c18dce3bea50b49e
Juudini <juudinidev@gmail.com>
2023-07-29 14:43:02 -0300

    Added new erorrs

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,5 +1,7 @@
 import { errorFactory } from "./errorHandler.js";
 
+// Products Errors
+
 export const DuplicatedProductError = errorFactory("DuplicatedProductError");
 export const ProductNotFoundError = errorFactory("ProductNotFound");
 export const DeleteProductError = errorFactory("DeleteProductError");
@@ -8,3 +10,8 @@ export const GetProductByIdError = errorFactory("GetProductByIdError");
 export const GetProductError = errorFactory("GetProductError");
 export const SaveToFileError = errorFactory("SaveToFileError");
 export const LoadFromFileError = errorFactory("LoadFromFileError");
+
+// Carts Errors
+export const GetCartByIdError = errorFactory("GetCartByIdError");
+export const AddNewCartError = errorFactory("AddNewCartError");
+export const CartNotFoundError = errorFactory("CartNotFoundError");
