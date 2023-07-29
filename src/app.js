a39fa61c7ce7310205747dc361dd78d60f4caa25
Juudini <juudinidev@gmail.com>
2023-07-29 14:47:05 -0300

    Added new app url

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import Express from "express";
 import { PORT } from "./config.js";
-import productRoutes from "./routes/products.routes.js";
+import productsRoutes from "./routes/products.routes.js";
+import cartsRoutes from "./routes/carts.routes.js";
 
 const app = Express();
 
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
-app.use("/api", productRoutes);
+app.use("/api", cartsRoutes);
+app.use("/api", productsRoutes);
+
 app.use((req, res, next) => {
     res.status(404).json({
         message: "Endpoint not found",
