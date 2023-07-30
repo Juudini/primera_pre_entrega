c03924c97666779b8fea78d0b8f49cb25c1b5f42
juudini <juudinidev@gmail.com>
2023-07-30 02:57:48 -0300

    Added new routes

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -1,11 +1,11 @@
 import { CartManager } from "../classes/CartManager.js";
 import { CartNotFoundError } from "../utils/errors.js";
-const cart = new CartManager();
+const manager = new CartManager();
 
-export const getCartById = async (req, res) => {
+export const getProducts = async (req, res) => {
     try {
         const cartId = req.params.cid;
-        const responseCart = await cart.getCartById(cartId);
+        const responseCart = await manager.getProductsInCart(cartId);
         res.send(responseCart);
     } catch (error) {
         if (error instanceof CartNotFoundError) {
@@ -19,8 +19,21 @@ export const getCartById = async (req, res) => {
 
 export const createCart = async (req, res) => {
     try {
-        const response = await cart.addNewCart();
+        const response = await manager.createCart();
         res.json(response);
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something goes wrong",
+        });
+    }
+};
+
+export const addProduct = async (req, res) => {
+    try {
+        const cartId = req.params.cid;
+        const productId = req.params.pid;
+        const response = await manager.addProductToCart(cartId, productId);
+        res.send(response);
     } catch (error) {
         return res.status(500).json({
             message: "Something goes wrong",
